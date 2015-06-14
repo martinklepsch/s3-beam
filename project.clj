d5d3132bde6b1e57e799ed17ca9d9f4f7bc52a11
Martin Klepsch <martinklepsch@googlemail.com>
2015-06-14 10:27:23 +0200

    bump to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.martinklepsch/s3-beam "0.3.0"
+(defproject org.martinklepsch/s3-beam "0.3.0-SNAPSHOT"
   :author "Martin Klepsch <http://www.martinklepsch.org>"
   :description "CORS Upload to S3 via Clojure(script)"
   :url "http://github.com/martinklepsch/s3-beam"
