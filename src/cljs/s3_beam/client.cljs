c4259dd2248d78c1d5d3eae04d65bc122729c926
Kenny Williams <kennyjwilli@gmail.com>
2015-11-25 14:28:48 -0800

    Formatting

--- src/cljs/s3_beam/client.cljs
+++ src/cljs/s3_beam/client.cljs
@@ -4,9 +4,9 @@
             [cljs.core.async :as async :refer [chan put! close! pipeline-async]]
             [goog.dom :as gdom]
             [goog.events :as events])
-  (:import  [goog Uri]
-            [goog.net XhrIo EventType ErrorCode]
-            [goog.events EventType]))
+  (:import [goog Uri]
+           [goog.net XhrIo EventType ErrorCode]
+           [goog.events EventType]))
 
 (defn file->map [f]
   {:name (.-name f)
@@ -28,33 +28,33 @@
   Alternatively, if a :key key exists in the input-map, use that in preference to the key-fn as an object-key.
   Sends the request to the server-url to be signed."
   [server-url edn-ize key-fn input-map-or-file ch]
-  (let [xhr      (XhrIo.)
+  (let [xhr (XhrIo.)
         {:keys [file identifier] :as input-map}
-                 (if (map? input-map-or-file)
-                   input-map-or-file
-                   {:file input-map-or-file})
-        fmap     (file->map file)
-        fmap     (cond
-                   (:key input-map) (assoc fmap :name (:key input-map))
-                   key-fn           (update-in fmap [:name] key-fn)
-                   :else            fmap)]
+        (if (map? input-map-or-file)
+          input-map-or-file
+          {:file input-map-or-file})
+        fmap (file->map file)
+        fmap (cond
+               (:key input-map) (assoc fmap :name (:key input-map))
+               key-fn (update-in fmap [:name] key-fn)
+               :else fmap)]
     (events/listen xhr goog.net.EventType.SUCCESS
-      (fn [_]
-        (put! ch {:f file
-                  :signature (edn-ize (.getResponseText xhr))
-                  :identifier identifier})
-        (close! ch)))
+                   (fn [_]
+                     (put! ch {:f          file
+                               :signature  (edn-ize (.getResponseText xhr))
+                               :identifier identifier})
+                     (close! ch)))
     (events/listen xhr goog.net.EventType.ERROR
-      (fn [_]
-        (let [error-code      (.getLastErrorCode xhr)
-              error-message   (.getDebugMessage goog.net.ErrorCode error-code)
-              http-error-code (.getStatus xhr)]
-          (put! ch {:identifier      identifier
-                    :error-code      error-code
-                    :error-message   (str "While trying to sign file: "
-                                           error-message)
-                    :http-error-code http-error-code})
-          (close! ch))))
+                   (fn [_]
+                     (let [error-code (.getLastErrorCode xhr)
+                           error-message (.getDebugMessage goog.net.ErrorCode error-code)
+                           http-error-code (.getStatus xhr)]
+                       (put! ch {:identifier      identifier
+                                 :error-code      error-code
+                                 :error-message   (str "While trying to sign file: "
+                                                       error-message)
+                                 :http-error-code http-error-code})
+                       (close! ch))))
     (. xhr send (signing-url server-url (:name fmap) (:type fmap)) "GET")))
 
 (defn formdata-from-map [m]
@@ -68,7 +68,7 @@
   [xml]
   (let [kv (->> (array-seq (.. xml -firstChild -childNodes))
                 (map (fn [n] [(.toLowerCase (.-nodeName n))
-                             (gdom/getTextContent n)])))]
+                              (gdom/getTextContent n)])))]
     (zipmap (map (comp keyword first) kv) (map second kv))))
 
 (defn upload-file
@@ -85,33 +85,33 @@
     (do
       (put! ch upload-info)
       (close! ch))
-    (let [xhr        (XhrIo.)
+    (let [xhr (XhrIo.)
           identifier (:identifier upload-info)
           sig-fields [:key :Content-Type :success_action_status :policy :AWSAccessKeyId :signature :acl]
-          signature  (select-keys (:signature upload-info) sig-fields)
-          form-data  (formdata-from-map (merge signature {:file (:f upload-info)}))]
+          signature (select-keys (:signature upload-info) sig-fields)
+          form-data (formdata-from-map (merge signature {:file (:f upload-info)}))]
       (events/listen xhr goog.net.EventType.SUCCESS
-        (fn [_]
-          (put! ch {:file       (:f upload-info)
-                    :response   (when-let [response-xml (.getResponseXml xhr)]
-                                    (xml->map response-xml))
-                    :xhr xhr
-                    :identifier identifier})
-          (close! ch)))
+                     (fn [_]
+                       (put! ch {:file       (:f upload-info)
+                                 :response   (when-let [response-xml (.getResponseXml xhr)]
+                                               (xml->map response-xml))
+                                 :xhr        xhr
+                                 :identifier identifier})
+                       (close! ch)))
       (events/listen xhr goog.net.EventType.ERROR
-        (fn [_]
-          (let [error-code      (.getLastErrorCode xhr)
-                error-message   (.getDebugMessage goog.net.ErrorCode error-code)
-                http-error-code (.getStatus xhr)]
-            (put! ch {:identifier      identifier
-                      :error-code      error-code
-                      :error-message   (str "While trying to upload file: "
-                                             error-message)
-                      :response   (when-let [response-xml (.getResponseXml xhr)]
-                                    (xml->map response-xml))
-                      :xhr xhr
-                      :http-error-code http-error-code})
-            (close! ch))))
+                     (fn [_]
+                       (let [error-code (.getLastErrorCode xhr)
+                             error-message (.getDebugMessage goog.net.ErrorCode error-code)
+                             http-error-code (.getStatus xhr)]
+                         (put! ch {:identifier      identifier
+                                   :error-code      error-code
+                                   :error-message   (str "While trying to upload file: "
+                                                         error-message)
+                                   :response        (when-let [response-xml (.getResponseXml xhr)]
+                                                      (xml->map response-xml))
+                                   :xhr             xhr
+                                   :http-error-code http-error-code})
+                         (close! ch))))
       (. xhr send (:action (:signature upload-info)) "POST" form-data))))
 
 (defn s3-pipe
@@ -125,10 +125,10 @@
                        defaults to nil, which means it will use the passed filename as the object key."
   ([report-chan] (s3-pipe report-chan {}))
   ([report-chan opts]
-   (let [opts       (merge {:server-url "/sign" :response-parser #(reader/read-string %)}
-                           opts)
+   (let [opts (merge {:server-url "/sign" :response-parser #(reader/read-string %)}
+                     opts)
          to-process (chan)
-         signed     (chan)]
+         signed (chan)]
      (pipeline-async 3
                      signed
                      (partial sign-file
