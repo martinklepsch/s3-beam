b532f0fbb0621118a0bcfcd9d8bfea814401d5f0
Daniel Compton <desk@danielcompton.net>
2016-02-18 16:29:17 +1300

    Add ring-codec as provided dependency

--- project.clj
+++ project.clj
@@ -11,7 +11,8 @@
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
                  ;[com.google.javascript/closure-compiler "v20140814"]
                  [org.clojure/data.json "0.2.5"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [ring/ring-codec "1.0.0" :scope "provided"]]
 
   :scm {:name "git"
          :url "https://github.com/martinklepsch/s3-beam"})
