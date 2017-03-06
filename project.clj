a9279e5fe308be52564b9c1c563e04eb13baa055
Daniel Compton <desk@danielcompton.net>
2017-03-07 08:37:37 +1300

    0.6.0-alpha3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.martinklepsch/s3-beam "0.6.0-alpha2"
+(defproject org.martinklepsch/s3-beam "0.6.0-alpha3"
   :author "Martin Klepsch <http://www.martinklepsch.org>"
   :description "CORS Upload to S3 via Clojure(script)"
   :url "http://github.com/martinklepsch/s3-beam"
